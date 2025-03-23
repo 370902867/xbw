d73e6b671a7f0378f6794dacdadc44339a7e3f72
370902867 <370902867@qq.com>
2025-03-23 09:04:29 +0800

    add

--- src/main/java/com/bj/books/entity/Book.java
+++ src/main/java/com/bj/books/entity/Book.java
@@ -10,4 +10,55 @@ public class Book {
     private String isbn;
     private Double price;
     private String description;
+
+    git config --global user.email "370902867@qq.com"
+    git config --global user.name "370902867"
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 } 
